992033f9fc5e2271a9a9e5afa9ff517adfd4af77
SuppresSF0rcE <suppressforce@gmail.com>
2017-04-13 00:06:21 +0200

    Init commit

---
+++ src/main/Main.java
@@ -0,0 +1,31 @@
+package main;
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * <code>public class Main extends Application</code><br>
+ * This class is called when you want to start program <br>
+ * It is firstly started in the software and contains the main method
+ * Created by SuppresSF0rcE on 4/12/17.
+ */
+public class Main extends Application {
+
+
+    /**
+     * The default method in the class it is firstly initialized and started on the beginning of the application
+     *
+     * @param args Arguments of the command line, currently they do nothing, can be ignored
+     */
+    public static void main(String[] args) {
+
+        //Launch JavaFx Application thread
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
